f609b85adb1b02464e983ac02aacbb7befc4cb26
Jose Carlos Gutiérrez <joseca20025@gmail.com>
2024-02-29 16:23:53 -0400

    Añadir funcionalidad de filtrado en el buscador

--- buscador/js/app.js
+++ buscador/js/app.js
@@ -1,7 +1,24 @@
 const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
+const precioMinimo = document.querySelector('#minimo');
+const precioMaximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
 const yearSelect = document.querySelector('#year');
 const maxYear = new Date().getFullYear();
 const minYear = maxYear - 10;
+
+let datosBusqueda = {
+  marca: '',
+  year: '',
+  precioMinimo: '',
+  precioMaximo: '',
+  puertas: '',
+  transmision: '',
+  color: '',
+};
 
 // * EVENTOS
 document.addEventListener('DOMContentLoaded', () => {
@@ -10,18 +27,106 @@ document.addEventListener('DOMContentLoaded', () => {
   llenarSelectYears();
 });
 
-const mostrarAutos = () => {
-  autos.forEach((auto) => {
-    const { marca, modelo, year, precio, puertas, color, transmision } = auto;
+marca.addEventListener('change', (event) => handleChange(event));
+year.addEventListener('change', (event) => handleChange(event));
+precioMinimo.addEventListener('change', (event) => handleChange(event));
+precioMaximo.addEventListener('change', (event) => handleChange(event));
+puertas.addEventListener('change', (event) => handleChange(event));
+transmision.addEventListener('change', (event) => handleChange(event));
+color.addEventListener('change', (event) => handleChange(event));
 
-    const autoHTML = document.createElement('p');
+const handleChange = ({ target }) => {
+  const { value, id } = target;
 
-    autoHTML.textContent = `
-    ${marca} ${modelo} - ${year} - ${puertas} puertas - Transmisión: ${transmision} - Color: ${color} - Precio: $${precio}
-    `;
+  console.log(value);
 
-    resultado.appendChild(autoHTML);
-  });
+  datosBusqueda = {
+    ...datosBusqueda,
+    [id]: value,
+  };
+
+  filtrarAuto();
+};
+
+const filtrarAuto = () => {
+  const resultado = autos.filter(filtrarMarca).filter(filtrarYear);
+
+  limpiarAutos();
+  mostrarAutos(resultado);
+};
+
+const filtrarMarca = (auto) => {
+  const { marca } = datosBusqueda;
+
+  if (marca) {
+    return auto.marca === marca;
+  }
+
+  return auto;
+};
+
+const filtrarYear = (auto) => {
+  const { year } = datosBusqueda;
+
+  if (year) {
+    return auto.year === parseInt(year);
+  }
+
+  return auto;
+};
+
+const filtrarPrecioMaximo = (auto) => {
+  const { precioMaximo } = datosBusqueda;
+
+  if (year) {
+    return auto.precioMaximo >= parseInt(precioMaximo);
+  }
+
+  return auto;
+};
+
+const filtrarPrecioMinimo = (auto) => {
+  const { precioMinimo } = datosBusqueda;
+
+  if (year) {
+    return auto.precioMinimo <= parseInt(precioMinimo);
+  }
+
+  return auto;
+};
+
+const mostrarAutos = (nuevosAutos) => {
+  if (!nuevosAutos) {
+    autos.forEach((auto) => {
+      const { marca, modelo, year, precio, puertas, color, transmision } = auto;
+
+      const autoHTML = document.createElement('p');
+
+      autoHTML.textContent = `
+      ${marca} ${modelo} - ${year} - ${puertas} puertas - Transmisión: ${transmision} - Color: ${color} - Precio: $${precio}
+      `;
+
+      resultado.appendChild(autoHTML);
+    });
+  } else {
+    nuevosAutos.forEach((auto) => {
+      const { marca, modelo, year, precio, puertas, color, transmision } = auto;
+
+      const autoHTML = document.createElement('p');
+
+      autoHTML.textContent = `
+      ${marca} ${modelo} - ${year} - ${puertas} puertas - Transmisión: ${transmision} - Color: ${color} - Precio: $${precio}
+      `;
+
+      resultado.appendChild(autoHTML);
+    });
+  }
+};
+
+const limpiarAutos = () => {
+  while (resultado.firstChild) {
+    resultado.firstChild.remove();
+  }
 };
 
 const llenarSelectYears = () => {
